4b5171570b1f190c822173073ad0a29f83296f85
Zoyn <602723113@qq.com>
2018-08-30 08:26:26 +0800

    Update: perfect the Basic
    Signed-off-by: Zoyn <602723113@qq.com>

--- common/src/main/java/cc/zoyn/core/util/BasicUtils.java
+++ common/src/main/java/cc/zoyn/core/util/BasicUtils.java
@@ -20,55 +20,25 @@ public final class BasicUtils {
     private BasicUtils() {
     }
 
-//    /**
-//     * 给一个玩家发送json数据
-//     *
-//     * @param player 玩家
-//     * @param msg    JSON数据
-//     */
-//    public static void sendJson(Player player, String msg) {
-//        String version = NMSUtils.getVersion();
-//        try {
-//            Object icbc = NMSUtils
-//                    .getNMSClass(version.equalsIgnoreCase("v1_8_R1") ? "ChatSerializer"
-//                            : "IChatBaseComponent$ChatSerializer")
-//                    .getMethod("a", new Class[]{String.class}).invoke(null, new Object[]{msg});
-//            Object ppoc = NMSUtils.getNMSClass("PacketPlayOutChat")
-//                    .getConstructor(new Class[]{NMSUtils.getNMSClass("IChatBaseComponent"), Byte.TYPE})
-//                    .newInstance(icbc, Byte.valueOf("1"));
-//            NMSUtils.sendPacket(player, ppoc);
-//        } catch (Exception e) {
-//            System.out.println("错误: " + e.getMessage());
-//        }
-//    }
-
     /**
-     * get server all online players
-     * @return
+     * Get the server all online players
+     *
+     * @return {@link List}
      */
     public static List<Player> getOnlinePlayers() {
-        // 实例化两个List用于存放Player和World
         List<Player> players = Lists.newArrayList();
-        List<World> worlds = Lists.newArrayList();
-        worlds.addAll(Bukkit.getWorlds());
-        // 遍历所有的世界
-        for (int i = 0; i < worlds.size(); i++) {
-            // 如果第i个世界的玩家是空的则进行下一次循环
-            if (!worlds.get(i).getPlayers().isEmpty()) {
-                // 不是空的则添加到players集合中
-                players.addAll(worlds.get(i).getPlayers());
-            }
-        }
+        List<World> worlds = Bukkit.getWorlds();
+        worlds.forEach(world -> players.addAll(world.getPlayers()));
         return players;
     }
 
     /**
      * 将玩家名转换为UUID
-     * <br />
-     * convert player name to UUID
+     * <p>
+     * Convert player name to UUID
      *
      * @param name the player name
-     * @return the name of the corresponding uuid
+     * @return {@link UUID}, the name of the corresponding uuid
      */
     public UUID translateNameToUUID(String name) {
         final Player player = Bukkit.getPlayerExact(name);
